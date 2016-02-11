abaa75f023f93c9540e56520ced9fc0e3cfd870c
Sudip Banerjee <>
2016-02-11 15:59:18 +0530

    Modifying acceptance test

--- tests/codeception/backend/tests/_bootstrap.php
+++ tests/codeception/backend/tests/_bootstrap.php
@@ -1,2 +1,6 @@
 <?php
 // This is global bootstrap for autoloading
+
+define('BASE_PATH', dirname(dirname(dirname(dirname(__DIR__)))));
+require(BASE_PATH .'/config.php');
+require(BASE_PATH .'/system/database.php');
--- tests/codeception/backend/tests/acceptance/UsersPageTableIsSortableCept.php
+++ tests/codeception/backend/tests/acceptance/UsersPageTableIsSortableCept.php
@@ -9,14 +9,62 @@ $I->wantTo('perform actions and see result');
 $I->actionLogin();
 $I->see('Users');
 $I->wait(1);
-$I->click('#fullname');
-$a = $I->grabAttributeFrom('//*[@id="fullname"]', 'class');
-$I->click('#fullname');
-$b = $I->grabAttributeFrom('//*[@id="fullname"]', 'class');
-if ($a != $b) {
-    $I->expect("Sorting Done");
-} else {
-    $I->see("Sorting was not performed");
+
+$randomFullName = rand(1,4);
+
+for($i=0;$i<$randomFullName;$i++){
+    $I->click('#fullname');
 }
 
+$fullnameDivClass = $I->grabAttributeFrom('//*[@id="fullname"]', 'class');
 
+if($fullnameDivClass !='sorting'){
+    //Taking Full name values from UI
+    $fullnameRowValue = $I->grabMultiple('.fullname');
+    $fullDetail = $fullname = array();
+    $flag = 0;
+    if($fullnameDivClass =='sorting_asc'){        
+        $fullDetail = get_all("SELECT full_name FROM users WHERE deleted=0 ORDER BY full_name asc");
+    }else{
+        $flag = 1;
+        $fullDetail = get_all("SELECT full_name FROM users WHERE deleted=0 ORDER BY full_name desc");
+    }
+    for($i=0; $i<count($fullDetail);$i++){
+        $fullname[] = $fullDetail[$i]["full_name"];
+    }
+    if($fullname === $fullnameRowValue){
+        \Codeception\Util\Debug::debug("Fullname is sorted in ". (($flag==0)?"ascending":"descending"));
+    }else{
+        \Codeception\Util\Debug::debug("Fullname is not sorted");
+    }
+}
+
+// Checking Username sorting.
+$randomUserName = rand(1,4);
+
+for($i=0;$i<$randomUserName;$i++){
+    $I->click('#username');
+}
+
+$usernameDivClass = $I->grabAttributeFrom('//*[@id="username"]', 'class');
+
+if($usernameDivClass !='sorting'){
+    //Taking Full name values from UI
+    $usernameRowValue = $I->grabMultiple('.username');
+    $usernameDetail = $username= array();
+    $flag = 0;
+    if($usernameDivClass =='sorting_asc'){        
+        $usernameDetail = get_all("SELECT username FROM users WHERE deleted=0 ORDER BY username asc");
+    }else{
+        $flag = 1;
+        $usernameDetail = get_all("SELECT username FROM users WHERE deleted=0 ORDER BY username desc");
+    }
+    for($i=0; $i<count($usernameDetail);$i++){
+        $username[] = $usernameDetail[$i]["username"];
+    }
+    if($username === $usernameRowValue){
+        \Codeception\Util\Debug::debug("Username is sorted in ". (($flag==0)?"ascending":"descending"));
+    }else{
+        \Codeception\Util\Debug::debug("Username is not sorted");
+    }
+}
